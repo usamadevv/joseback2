6e90c015bd4926972a49f2de605d7a70aa46f6c3
MacBook <salemmjadoon766@gmail.com>
2023-12-13 16:03:00 +0500

    Your commit message here

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 const PORT =  process.env.PORT || 4001;
 const path=require("path");
+
+
+const http = require('http');
+const {Server} = require('socket.io');
+
+
+  
 app.use(bodyParser.json({limit: '35mb'}));
 
     app.use(
@@ -34,7 +40,6 @@ const Supervisorroute = require('./Routes/Supervisor.route');
 const Invoiceroute = require('./Routes/Invoice.route');
 const Formroute = require('./Routes/Formdata');
 __dirname=path.resolve()
-app.use(express.static(path.join(__dirname,'./myapp/build')))
 app.use(cors({origin: '*'}));
 app.use(function (req, res, next) {
 
@@ -54,10 +59,6 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-
-
-
-
 app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect(
@@ -92,14 +93,50 @@ app.use('/api/invoice',Invoiceroute  );
 app.use('/api/data',Formroute  );
 
 
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  });
+const userRooms = {};
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    
+    const { email, room,sender } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+
+    io.emit("room:callto", data);
+  });
+  
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
 
 
 
-
-app.get('*',(req,res)=>{
-    res.sendFile( path.resolve(__dirname,'./myapp','build','index.html'))
-
-})
-app.listen(PORT, function() {
+server.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
